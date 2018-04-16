6897252b2345882486ec21fc36a746f8863b6f1d
sajedulislam <sislam@isrt.ac.bd>
2018-04-16 21:44:50 +0600

    Update help.js

--- MIT Project/js/help.js
+++ MIT Project/js/help.js
@@ -3,11 +3,6 @@
 
 var HELP = new HELP_CLASS();
 
-/** 
- * manages help actions
- * 
- * @author ViliusL
- */
 function HELP_CLASS() {
 	
 	//shortcuts
@@ -33,34 +28,13 @@ function HELP_CLASS() {
 	};
 	//about
 	this.help_about = function () {
-		var email = 'www.viliusl@gmail.com';
-		POP.add({title: "Name:", value: "miniPaint"});
+		var email = 'sajedul.islam.tushar@gmail.com';
+		POP.add({title: "Name:", value: "MIT Project - Online Graphic Designer and Photo Editor"});
 		POP.add({title: "Version:", value: VERSION});
-		POP.add({title: "Author:", value: 'ViliusL'});
+		POP.add({title: "Author:", value: 'Sajedul Islam'});
 		POP.add({title: "Email:", html: '<a href="mailto:' + email + '">' + email + '</a>'});
-		POP.add({title: "GitHub:", html: '<a href="https://github.com/viliusle/miniPaint">http://github.com/viliusle/miniPaint</a>'});
+		POP.add({title: "GitHub:", html: '<a href="https://sajedulislam.github.io/MIT%20Project/index.html">https://sajedulislam.github.io/MIT%20Project/index.html</a>'});
 		POP.show('About', '');
 	};
 	
-	//change language
-	this.help_translate = function (lang_code) {
-		
-		//default English emty translator
-		var dict_en = {};
-		
-		//save cookie
-		if(lang_code != undefined && lang_code != LANG){
-			HELPER.setCookie('language', lang_code);
-		}
-		
-		var dictionary_data = "dict_"+lang_code;
-		if(window[dictionary_data] != undefined || lang_code == 'en'){
-			//translate
-			$('body').translate({lang: lang_code, t: window[dictionary_data]});
-			LANG = lang_code;	
-		}
-		else{
-			console.log('Translate error, can not find dictionary: '+dictionary_data);
-		}
-	};
 }
