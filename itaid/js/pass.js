eede3e2e80ca855e9a9f930be755480693cc7db4
sajedulislam <sislam@isrt.ac.bd>
2022-02-03 11:21:17 +0600

    Add files via upload

---
+++ itaid/js/pass.js
@@ -0,0 +1,8 @@
+var password = prompt("Enter the password")
+if (password == "it2020") {
+alert("Welcome to ITAID!")
+}
+else {
+alert("Wrong Password, Redirecting you to GOOGLE!")
+location = "https://www.google.com/"
+}
