10c68fc7e8d31572dbe3ba58eeaf3825a9e292f9
sajedulislam <sislam@isrt.ac.bd>
2022-04-10 16:37:24 +0600

    Delete itaid directory

--- itaid/js/pass.js
+++
@@ -1,8 +0,0 @@
-var password = prompt("Enter the password")
-if (password == "it2022") {
-alert("Welcome to ITAID!")
-}
-else {
-alert("Wrong Password, Redirecting you to GOOGLE!")
-location = "https://www.google.com/"
-}
