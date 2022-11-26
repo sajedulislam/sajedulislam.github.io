36216ca95cb8739142d1174c50b6f8b7d4bda120
Sajedul Islam <sislam@isrt.ac.bd>
2022-11-26 15:42:07 +0600

    Add files via upload

---
+++ music/js/music-list.js
@@ -0,0 +1,58 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Tere Sang Yaara",
+    artist: "Rustom 2016",
+    img: "cover",
+    src: "music-1"
+  },
+  {
+    name: "Dil Ko Karaar Aaya",
+    artist: "Neha Kakkar",
+    img: "cover",
+    src: "music-2"
+  },
+  {
+    name: "Kusu Kusu",
+    artist: "Satyameva Jayate 2",
+    img: "cover",
+    src: "music-3"
+  },
+  {
+    name: "Meri Zindagi Hai Tu",
+    artist: "Satyameva Jayate 2",
+    img: "cover",
+    src: "music-4"
+  },
+  {
+    name: "Raatan Lambiyan",
+    artist: "Sershah",
+    img: "cover",
+    src: "music-5"
+  },
+    {
+    name: "Tumko Barish Pasand Hai",
+    artist: "Neha Kakkar",
+    img: "cover",
+    src: "music-6"
+  },
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
