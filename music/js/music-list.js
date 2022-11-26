7a567698a717541a5c0c4c802c250d5b9cc28ae6
Sajedul Islam <sislam@isrt.ac.bd>
2022-11-26 20:52:30 +0600

    Add files via upload

---
+++ music/js/music-list.js
@@ -0,0 +1,212 @@
+b --// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Tere Sang Yaara",
+    artist: "Rustom 2016",
+    img: "cover",
+    src: "music-1"
+  },
+  {
+    name: "Dil Ko Karaar Aaya",
+    artist: "Neha Kakkar",
+    img: "cover",
+    src: "music-2"
+  },
+  {
+    name: "Kusu Kusu",
+    artist: "Satyameva Jayate 2",
+    img: "cover",
+    src: "music-3"
+  },
+  {
+    name: "Meri Zindagi Hai Tu",
+    artist: "Satyameva Jayate 2",
+    img: "cover",
+    src: "music-4"
+  },
+  {
+    name: "Raatan Lambiyan",
+    artist: "Sershah",
+    img: "cover",
+    src: "music-5"
+  },
+    {
+    name: "Tumko Barish Pasand Hai",
+    artist: "Neha Kakkar",
+    img: "cover",
+    src: "music-6"
+  },
+    {
+    name: "A thousand years",
+    artist: "Christina Perri",
+    img: "cover",
+    src: "music-7"
+  },
+      {
+    name: "Aj raate kono rupkotha nei",
+    artist: "Old school",
+    img: "cover",
+    src: "music-8"
+  },
+        {
+    name: "Alo Aloo",
+    artist: "Tahsan",
+    img: "cover",
+    src: "music-9"
+  },
+          {
+    name: "Ami megh chure dei",
+    artist: "Dhrubo",
+    img: "cover",
+    src: "music-10"
+  },
+            {
+    name: "Bhalobasha Tar Por",
+    artist: "Arnob",
+    img: "cover",
+    src: "music-11"
+  },
+            {
+    name: "Tor Jonno Ami Bonno",
+    artist: "Fuad",
+    img: "cover",
+    src: "music-12"
+  },
+            {
+    name: "Jadur Sohor",
+    artist: "Chirkut",
+    img: "cover",
+    src: "music-13"
+  },
+              {
+    name: "Sharthopor",
+    artist: "Jaal The Trap",
+    img: "cover",
+    src: "music-14"
+  },
+              {
+    name: "Shihoron",
+    artist: "Miftah Zaman",
+    img: "cover",
+    src: "music-15"
+  },
+                {
+    name: "Khola Janala",
+    artist: "SWAT",
+    img: "cover",
+    src: "music-16"
+  },
+    {
+    name: "Where do u go",
+    artist: "No Mercy",
+    img: "cover",
+    src: "music-17"
+  },
+  {
+    name: "Watch Over You",
+    artist: "Blackbird",
+    img: "cover",
+    src: "music-18"
+  },
+  {
+    name: "Purnota",
+    artist: "Warfaze",
+    img: "cover",
+    src: "music-19"
+  },
+  {
+    name: "Shey Raate",
+    artist: "Artcell",
+    img: "cover",
+    src: "music-20"
+  },
+    {
+    name: "Whenever whenever",
+    artist: "Shakira",
+    img: "cover",
+    src: "music-21"
+  },
+    {
+    name: "QUIT PLAYING GAMES",
+    artist: "Backstreet Boys",
+    img: "cover",
+    src: "music-22"
+  },
+      {
+    name: "Hariye Giyechi",
+    artist: "Arnob",
+    img: "cover",
+    src: "music-23"
+  },
+        {
+    name: "Hot N Cold",
+    artist: "Katy Perry",
+    img: "cover",
+    src: "music-24"
+  },
+          {
+    name: "Nikosh kalo",
+    artist: "Paper Rhyme",
+    img: "cover",
+    src: "music-25"
+  },
+            {
+    name: "Shahar Bapi Bari Jaa",
+    artist: "Arjun Mimi Bonnie Jeet Gannguli",
+    img: "cover",
+    src: "music-26"
+  },
+            {
+    name: "Nobina",
+    artist: "Fuad_Ft_Poonam",
+    img: "cover",
+    src: "music-27"
+  },
+            {
+    name: "One Love",
+    artist: "Blue",
+    img: "cover",
+    src: "music-28"
+  },
+              {
+    name: "She Je Boshe Ache",
+    artist: "Arnob",
+    img: "cover",
+    src: "music-29"
+  },
+              {
+    name: "Nitol Paye",
+    artist: "Fuad",
+    img: "cover",
+    src: "music-30"
+  },
+                {
+    name: "Love The Way You Lie",
+    artist: "Eminem -  ft. Rihanna",
+    img: "cover",
+    src: "music-31"
+  },
+    name: "Himaloy",
+    artist: "Debi",
+    img: "cover",
+    src: "music-32"
+  },
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
